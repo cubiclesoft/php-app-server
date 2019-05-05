41b0d452916f76d7db98dd2ebed0d9d6321432eb
cubiclesoft <webmaster@cubiclesoft.com>
2019-05-05 07:05:08 -0700

    Latest libraries.

--- support/process_helper.php
+++ support/process_helper.php
@@ -132,6 +132,124 @@
 			return $result;
 		}
 
+		public static function StartTCPServer()
+		{
+			if (self::$serverfp === false)
+			{
+				// Oddly, this server starts up in about 0.002 seconds BUT calling fclose() on this handle takes 0.5 seconds.
+				// So it doesn't really hurt to keep the server alive.
+				self::$serverfp = stream_socket_server("tcp://127.0.0.1:0", $errornum, $errorstr, STREAM_SERVER_BIND | STREAM_SERVER_LISTEN);
+				if (self::$serverfp === false)  return array("success" => false, "error" => self::PHTranslate("Failed to start localhost TCP/IP server."), "errorcode" => "bind_failed");
+
+				stream_set_blocking(self::$serverfp, 0);
+			}
+
+			$info = stream_socket_get_name(self::$serverfp, false);
+			$pos = strrpos($info, ":");
+			$ip = substr($info, 0, $pos);
+			$port = (int)substr($info, $pos + 1);
+
+			if (!class_exists("CSPRNG", false))  $token = bin2hex(random_bytes(64));
+			else
+			{
+				$rng = new CSPRNG();
+				$token = $rng->GenerateToken();
+			}
+
+			return array("success" => true, "fp" => self::$serverfp, "ip" => $ip, "port" => $port, "token" => $token);
+		}
+
+		public static function GetTCPPipes(&$pipes, $servertoken, $proc, $waitfor = 0.5, $checkcallback = false)
+		{
+			$pipesleft = 0;
+			foreach ($pipes as $val)
+			{
+				if ($val === false)  $pipesleft++;
+			}
+
+			$servertokenlen = strlen($servertoken);
+
+			$ts = microtime(true);
+			$clients = array();
+			while ($pipesleft)
+			{
+				$readfps = array(self::$serverfp);
+				foreach ($clients as $client)  $readfps[] = $client->fp;
+				$writefps = array();
+				$exceptfps = NULL;
+				$result = @stream_select($readfps, $writefps, $exceptfps, 1);
+				if ($result === false)  break;
+
+				if (in_array(self::$serverfp, $readfps) && ($fp = stream_socket_accept(self::$serverfp)) !== false)
+				{
+					stream_set_blocking($fp, 0);
+
+					$client = new stdClass();
+					$client->fp = $fp;
+					$client->data = "";
+
+					$clients[] = $client;
+				}
+
+				foreach ($clients as $num => $client)
+				{
+					$data = @fread($client->fp, $servertokenlen + 1 - strlen($client->data));
+					if ($data === false || ($data === "" && feof($client->fp)))
+					{
+						fclose($client->fp);
+
+						unset($clients[$num]);
+					}
+					else
+					{
+						$client->data .= $data;
+
+						if (strlen($client->data) == $servertokenlen + 1)
+						{
+							// Compare the input token to the one sent to the process.
+							if (self::CTstrcmp($servertoken, substr($client->data, 0, -1)))  fclose($client->fp);
+							else
+							{
+								$num2 = ord(substr($client->data, -1));
+
+								if (!isset($pipes[$num2]) || $pipes[$num2] !== false)  fclose($client->fp);
+								else
+								{
+									$pipes[$num2] = $fp;
+
+									$pipesleft--;
+
+									$ts = microtime(true);
+								}
+							}
+
+							unset($clients[$num]);
+						}
+					}
+				}
+
+				// If the process died, then bail out.
+				if ($pipesleft && microtime(true) - $ts > $waitfor)
+				{
+					if ($proc !== false)
+					{
+						$pinfo = @proc_get_status($proc);
+						if (!$pinfo["running"])  break;
+					}
+					else if (!is_callable($checkcallback) || !call_user_func_array($checkcallback, array($pipes)))
+					{
+						break;
+					}
+
+					$ts = microtime(true);
+				}
+			}
+
+			if ($pipesleft)  return array("success" => false, "error" => self::PHTranslate("The process started but failed to connect to the localhost TCP/IP server before terminating."), "errorcode" => "broken_tcp_pipe");
+
+			return array("success" => true);
+		}
+
 		// Starts a process with non-blocking pipes.  Windows may require 'createprocess.exe' from:  https://github.com/cubiclesoft/createprocess-windows
 		// Non-blocking is required for scenarios when using more than one pipe or a deadlock can happen.
 		// For example, one process blocks on stdin to be read while another is blocking on stdout to be read.
@@ -144,44 +262,56 @@
 			$procpipes = array();
 
 			if (!isset($options["stdin"]))  $options["stdin"] = true;
-			if (is_string($options["stdin"]))  $procpipes[] = array("file", $options["stdin"], "r");
-			else if (is_resource($options["stdin"]) || is_array($options["stdin"]))  $procpipes[] = $options["stdin"];
-			else if ($options["stdin"] === false)  $procpipes[] = array("file", ($windows ? "NUL" : "/dev/null"), "r");
-			else  $procpipes[] = array("pipe", "r");
-
 			if (!isset($options["stdout"]))  $options["stdout"] = true;
-			if (is_string($options["stdout"]))  $procpipes[] = array("file", $options["stdout"], "w");
-			else if (is_resource($options["stdout"]) || is_array($options["stdout"]))  $procpipes[] = $options["stdout"];
-			else if ($options["stdout"] === false)  $procpipes[] = array("file", ($windows ? "NUL" : "/dev/null"), "w");
-			else  $procpipes[] = array("pipe", "w");
-
 			if (!isset($options["stderr"]))  $options["stderr"] = true;
-			if (is_string($options["stderr"]))  $procpipes[] = array("file", $options["stderr"], "w");
-			else if (is_resource($options["stderr"]) || is_array($options["stderr"]))  $procpipes[] = $options["stderr"];
-			else if ($options["stderr"] === false)  $procpipes[] = array("file", ($windows ? "NUL" : "/dev/null"), "w");
-			else  $procpipes[] = array("pipe", "w");
+
+			if (!is_string($options["stdin"]) || $options["stdin"] !== "")
+			{
+				if (is_string($options["stdin"]))  $procpipes[0] = array("file", $options["stdin"], "r");
+				else if (is_resource($options["stdin"]) || is_array($options["stdin"]))  $procpipes[0] = $options["stdin"];
+				else if ($options["stdin"] === false)  $procpipes[0] = array("file", ($windows ? "NUL" : "/dev/null"), "r");
+				else  $procpipes[0] = array("pipe", "r");
+			}
+
+			if (!is_string($options["stdout"]) || $options["stdout"] !== "")
+			{
+				if (is_string($options["stdout"]))  $procpipes[1] = array("file", $options["stdout"], "w");
+				else if (is_resource($options["stdout"]) || is_array($options["stdout"]))  $procpipes[1] = $options["stdout"];
+				else if ($options["stdout"] === false)  $procpipes[1] = array("file", ($windows ? "NUL" : "/dev/null"), "w");
+				else  $procpipes[1] = array("pipe", "w");
+			}
+
+			if (!is_string($options["stderr"]) || $options["stderr"] !== "")
+			{
+				if (is_string($options["stderr"]))  $procpipes[2] = array("file", $options["stderr"], "w");
+				else if (is_resource($options["stderr"]) || is_array($options["stderr"]))  $procpipes[2] = $options["stderr"];
+				else if ($options["stderr"] === false)  $procpipes[2] = array("file", ($windows ? "NUL" : "/dev/null"), "w");
+				else  $procpipes[2] = array("pipe", "w");
+			}
 
 			// Windows requires redirecting pipes through sockets so they can be configured to be non-blocking.
 			if ($windows)
 			{
 				// Don't open a socket if the application really does want a pipe.
-				if (!isset($options["tcpstdin"]))  $options["tcpstdin"] = true;
+				if (!isset($options["tcpstdin"]))  $options["tcpstdin"] = false;
 				if (!isset($options["tcpstdout"]))  $options["tcpstdout"] = true;
 				if (!isset($options["tcpstderr"]))  $options["tcpstderr"] = true;
-				$tcpused = ((is_array($procpipes[0]) && $procpipes[0][0] === "pipe" && $options["tcpstdin"]) || (is_array($procpipes[1]) && $procpipes[1][0] === "pipe" && $options["tcpstdout"]) || (is_array($procpipes[2]) && $procpipes[2][0] === "pipe" && $options["tcpstderr"]));
+				$tcpused = ((isset($procpipes[0]) && is_array($procpipes[0]) && $procpipes[0][0] === "pipe" && $options["tcpstdin"]) || (isset($procpipes[1]) && is_array($procpipes[1]) && $procpipes[1][0] === "pipe" && $options["tcpstdout"]) || (isset($procpipes[2]) && is_array($procpipes[2]) && $procpipes[2][0] === "pipe" && $options["tcpstderr"]));
 
 				// Attempt to locate 'createprocess.exe'.
 				if (isset($options["createprocess_exe"]) && !file_exists($options["createprocess_exe"]))  unset($options["createprocess_exe"]);
 
 				if (!isset($options["createprocess_exe"]))
 				{
-					$filename = str_replace("\\", "/", dirname(__FILE__)) . "/createprocess.exe";
+					$filename = str_replace("\\", "/", dirname(__FILE__)) . "/windows/createprocess.exe";
+					if (!file_exists($filename))  $filename = str_replace("\\", "/", dirname(__FILE__)) . "/createprocess.exe";
 					if (file_exists($filename))  $options["createprocess_exe"] = $filename;
 				}
 
 				if (!isset($options["createprocess_exe"]))
 				{
-					$filename = str_replace("\\", "/", dirname(__FILE__)) . "/createprocess-win.exe";
+					$filename = str_replace("\\", "/", dirname(__FILE__)) . "/windows/createprocess-win.exe";
+					if (!file_exists($filename))  $filename = str_replace("\\", "/", dirname(__FILE__)) . "/createprocess-win.exe";
 					if (file_exists($filename))  $options["createprocess_exe"] = $filename;
 				}
 
@@ -192,33 +322,16 @@
 
 				if ($tcpused)
 				{
-					if (self::$serverfp === false)
-					{
-						// Oddly, this server starts up in about 0.002 seconds BUT calling fclose() on this handle takes 0.5 seconds.
-						// So it doesn't really hurt to keep the server alive.
-						self::$serverfp = stream_socket_server("tcp://127.0.0.1:0", $errornum, $errorstr, STREAM_SERVER_BIND | STREAM_SERVER_LISTEN);
-						if (self::$serverfp === false)  return array("success" => false, "error" => self::PHTranslate("Failed to start localhost TCP/IP server."), "errorcode" => "bind_failed");
+					$result = self::StartTCPServer();
+					if (!$result["success"])  return $result;
 
-						stream_set_blocking(self::$serverfp, 0);
-					}
-
-					$serverinfo = stream_socket_get_name(self::$serverfp, false);
-					$pos = strrpos($serverinfo, ":");
-					$serverip = substr($serverinfo, 0, $pos);
-					$serverport = (int)substr($serverinfo, $pos + 1);
-
-					if (!class_exists("CSPRNG", false))  $servertoken = bin2hex(random_bytes(64));
-					else
-					{
-						$rng = new CSPRNG();
-						$servertoken = $rng->GenerateToken();
-					}
-					$servertokenlen = strlen($servertoken);
+					$serverport = $result["port"];
+					$servertoken = $result["token"];
 
 					$cmd2 .= " /socketip=127.0.0.1 /socketport=" . $serverport . " /sockettoken=" . $servertoken;
-					if (is_array($procpipes[0]) && $procpipes[0][0] === "pipe" && $options["tcpstdin"])  $cmd2 .= " /stdin=socket";
-					if (is_array($procpipes[1]) && $procpipes[1][0] === "pipe" && $options["tcpstdout"])  $cmd2 .= " /stdout=socket";
-					if (is_array($procpipes[2]) && $procpipes[2][0] === "pipe" && $options["tcpstderr"])  $cmd2 .= " /stderr=socket";
+					if (isset($procpipes[0]) && is_array($procpipes[0]) && $procpipes[0][0] === "pipe" && $options["tcpstdin"])  $cmd2 .= " /stdin=socket";
+					if (isset($procpipes[1]) && is_array($procpipes[1]) && $procpipes[1][0] === "pipe" && $options["tcpstdout"])  $cmd2 .= " /stdout=socket";
+					if (isset($procpipes[2]) && is_array($procpipes[2]) && $procpipes[2][0] === "pipe" && $options["tcpstderr"])  $cmd2 .= " /stderr=socket";
 				}
 
 				$cmd = $cmd2 . " " . $cmd;
@@ -263,97 +376,32 @@
 			// Rebuild the pipes on Windows by waiting for a valid inbound TCP/IP connection for each pipe.
 			if ($windows && $tcpused)
 			{
-				$pipesleft = 0;
-				if (is_array($procpipes[0]) && $procpipes[0][0] === "pipe" && $options["tcpstdin"])
+				if (isset($procpipes[0]) && is_array($procpipes[0]) && $procpipes[0][0] === "pipe" && $options["tcpstdin"])
 				{
 					fclose($pipes[0]);
 
 					$pipes[0] = false;
-					$pipesleft++;
 				}
-				if (is_array($procpipes[1]) && $procpipes[1][0] === "pipe" && $options["tcpstdout"])
+				if (isset($procpipes[1]) && is_array($procpipes[1]) && $procpipes[1][0] === "pipe" && $options["tcpstdout"])
 				{
 					fclose($pipes[1]);
 
 					$pipes[1] = false;
-					$pipesleft++;
 				}
-				if (is_array($procpipes[2]) && $procpipes[2][0] === "pipe" && $options["tcpstderr"])
+				if (isset($procpipes[2]) && is_array($procpipes[2]) && $procpipes[2][0] === "pipe" && $options["tcpstderr"])
 				{
 					fclose($pipes[2]);
 
 					$pipes[2] = false;
-					$pipesleft++;
 				}
 
-				$ts = microtime(true);
-				$clients = array();
-				while ($pipesleft)
+				$result = self::GetTCPPipes($pipes, $servertoken, $proc);
+				if (!$result["success"])
 				{
-					$readfps = array(self::$serverfp);
-					foreach ($clients as $client)  $readfps[] = $client->fp;
-					$writefps = array();
-					$exceptfps = NULL;
-					$result = @stream_select($readfps, $writefps, $exceptfps, 1);
-					if ($result === false)  break;
+					$result["info"] = array("cmd" => $cmd, "dir" => (isset($options["dir"]) ? $options["dir"] : NULL), "env" => $options["env"]);
 
-					if (in_array(self::$serverfp, $readfps) && ($fp = stream_socket_accept(self::$serverfp)) !== false)
-					{
-						stream_set_blocking($fp, 0);
-
-						$client = new stdClass();
-						$client->fp = $fp;
-						$client->data = "";
-
-						$clients[] = $client;
-					}
-
-					foreach ($clients as $num => $client)
-					{
-						$data = @fread($client->fp, $servertokenlen + 1 - strlen($client->data));
-						if ($data === false || ($data === "" && feof($client->fp)))
-						{
-							fclose($client->fp);
-
-							unset($clients[$num]);
-						}
-						else
-						{
-							$client->data .= $data;
-
-							if (strlen($client->data) == $servertokenlen + 1)
-							{
-								// Compare the input token to the one sent to the process.
-								if (self::CTstrcmp($servertoken, substr($client->data, 0, -1)))  fclose($client->fp);
-								else
-								{
-									$num2 = ord(substr($client->data, -1));
-
-									if (!isset($pipes[$num2]) || $pipes[$num2] !== false)  fclose($client->fp);
-									else
-									{
-										$pipes[$num2] = $fp;
-
-										$pipesleft--;
-									}
-								}
-
-								unset($clients[$num]);
-							}
-						}
-					}
-
-					// If the process died, then bail out.
-					if ($pipesleft && microtime(true) - $ts > 0.5)
-					{
-						$pinfo = @proc_get_status($proc);
-						if (!$pinfo["running"])  break;
-
-						$ts = microtime(true);
-					}
+					return $result;
 				}
-
-				if ($pipesleft)  return array("success" => false, "error" => self::PHTranslate("The process started but failed to connect to the localhost TCP/IP server before terminating."), "errorcode" => "broken_tcp_pipe", "info" => array("cmd" => $cmd, "dir" => (isset($options["dir"]) ? $options["dir"] : NULL), "env" => $options["env"]));
 			}
 
 			// Change all pipes to non-blocking.
@@ -365,6 +413,105 @@
 			$pinfo = @proc_get_status($proc);
 
 			return array("success" => true, "proc" => $proc, "pid" => $pinfo["pid"], "pipes" => $pipes, "info" => array("cmd" => $cmd, "dir" => (isset($options["dir"]) ? $options["dir"] : NULL), "env" => $options["env"]));
+		}
+
+		public static function Wait($proc, &$pipes, $stdindata = "", $timeout = -1)
+		{
+			$stdindata = (string)$stdindata;
+			$stdoutdata = "";
+			$stderrdata = "";
+
+			$startts = microtime(true);
+			do
+			{
+				$readfps = array();
+				if (isset($pipes[1]))  $readfps[] = $pipes[1];
+				if (isset($pipes[2]))  $readfps[] = $pipes[2];
+
+				$writefps = array();
+				if (isset($pipes[0]))
+				{
+					if ($stdindata !== "")  $writefps[] = $pipes[0];
+					else
+					{
+						fclose($pipes[0]);
+
+						unset($pipes[0]);
+					}
+				}
+
+				$ts = microtime(true);
+				if ($timeout < 0)  $timeleft = false;
+				else
+				{
+					$timeleft = $timeout - ($ts - $startts);
+					if ($timeleft < 0)  $timeleft = 0;
+				}
+
+				if (!count($readfps) && !count($writefps))  usleep(($timeleft !== false && $timeleft < 0.25 ? $timeleft : 250000));
+				else
+				{
+					$exceptfps = NULL;
+					$result = @stream_select($readfps, $writefps, $exceptfps, ($timeleft > 1 ? 1 : 0), ($timeleft > 1 ? 0 : ($timeleft - (int)$timeleft) * 1000000));
+					if ($result === false)  break;
+
+					// Handle stdin.
+					if (isset($pipes[0]) && $stdindata !== "")
+					{
+						$result = @fwrite($pipes[0], substr($stdindata, 0, 4096));
+						if ($result)  $stdindata = (string)substr($stdindata, $result);
+					}
+
+					// Handle stdout.
+					if (isset($pipes[1]))
+					{
+						$data = @fread($pipes[1], 65536);
+						if ($data === false || ($data === "" && feof($pipes[1])))
+						{
+							fclose($pipes[1]);
+
+							unset($pipes[1]);
+						}
+						else
+						{
+							$stdoutdata .= $data;
+						}
+					}
+
+					// Handle stderr.
+					if (isset($pipes[2]))
+					{
+						$data = @fread($pipes[2], 65536);
+						if ($data === false || ($data === "" && feof($pipes[2])))
+						{
+							fclose($pipes[2]);
+
+							unset($pipes[2]);
+						}
+						else
+						{
+							$stderrdata .= $data;
+						}
+					}
+				}
+
+				// Verify that the process is stll running.
+				if ($proc !== false)
+				{
+					$pinfo = @proc_get_status($proc);
+					if (!$pinfo["running"])
+					{
+						if (isset($pipes[0]))  fclose($pipes[0]);
+						unset($pipes[0]);
+
+						$proc = false;
+					}
+				}
+
+				if ($timeleft === 0)  break;
+			} while ($proc !== false || count($pipes));
+
+			return array("success" => true, "proc" => $proc, "stdinleft" => $stdindata, "stdout" => $stdoutdata, "stderr" => $stderrdata);
 		}
 
 		public static function TerminateProcess($id, $children = true, $force = true)
